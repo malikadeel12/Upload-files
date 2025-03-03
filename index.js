8556eac9bff8a99c165b2bea28e189d68d1f1bbb
Innocent boy <nomanriaz7980@gmail.com>
2025-03-03 01:19:59 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const express= require("express");
+const multer  = require('multer');
+
+const app = express();
+const PORT = 4000;
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, `${Date.now()}-${file.originalname}`)
+    }
+  });
+   const upload = multer({ storage: storage });
+
+app.set("view engine", "ejs");
+app.set("Views" ,path.resolve("./Views"));
+
+app.use(express.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+  return  res.render("homepage");
+});  
+app.post('/profile', upload.single('profileImage'), function (req, res, next) {
+console.log(req.body);
+console.log(req.file);
+return res.render("homepage");
+  })  ;
+
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on port" + PORT);
+});
